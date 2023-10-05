e11be3b465cee2407a801ba9a5edb0ba6761a19d
M Zaenal Mustofa <zendzzo@gmail.com>
2023-10-05 09:22:39 +0700

    Install spatie/laravel-permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,10 +14,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Collection;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements HasTenants
 {
-  use HasApiTokens, HasFactory, Notifiable;
+  use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
   /**
    * The attributes that are mass assignable.
